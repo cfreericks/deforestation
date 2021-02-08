b6ca96272e84863153b7f14fdf1b8ccd2edb5f3a
cfreericks <admin@velomalist.com>
2021-02-08 14:31:00 +0100

    Implemented initial sourcing of working scripts.

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,20 @@
-## This master script is responsible for launching and orchestrating the
-## functions defined in the src folder
+# This master script is responsible for launching and orchestrating the
+# functions defined in the src folder
 
-# Load libraries
+
+# Load libraries ----------------------------------------------------------
+
 library(here)
 
-source("./src/data_engineering.R")
 
-sayhello("Icke")
+# Load scripts ------------------------------------------------------------
+
+# "01_data_engineering.R", 
+# "02_factoranalysis.R", 
+# "03_clusterBeschreiben.R", 
+# "04_regressionanalysis.R"
+
+path = here("src/")
+pathnames <- list.files(pattern = "[.]R$", path = path, full.names = TRUE)
+
+sapply(pathnames, FUN = source)
